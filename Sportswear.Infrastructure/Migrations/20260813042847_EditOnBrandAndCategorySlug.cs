094f2244d9a90060bb32e2246514dfcbc290c52a
Mostafa <mostafasalba2020@gmail.com>
2026-08-13 07:30:31 +0300

    Add Index to slug

---
+++ Sportswear.Infrastructure/Migrations/20260813042847_EditOnBrandAndCategorySlug.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sportswear.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditOnBrandAndCategorySlug : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_Slug",
+                table: "Categories",
+                column: "Slug",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Brands_Slug",
+                table: "Brands",
+                column: "Slug",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_Slug",
+                table: "Categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Brands_Slug",
+                table: "Brands");
+        }
+    }
+}
